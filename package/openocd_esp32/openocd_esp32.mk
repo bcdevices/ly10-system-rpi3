917ac62d41cf2bd203970d104ef257d9311455b4
Eric Rauer <eric@bcdevices.com>
2018-05-17 21:14:09 -0700

    Change openocd esp32 to autoconf

--- package/openocd_esp32/openocd_esp32.mk
+++ package/openocd_esp32/openocd_esp32.mk
@@ -13,25 +13,13 @@ OPENOCD_ESP32_AUTORECONF = YES
 OPENOCD_ESP32_DEPENDENCIES = libusb-compat
 OPENOCD_ESP32_DEPENDENCIES += libftdi
 
-OPENOCD_ESP32_CONF_OPTS = --prefix=/usr/local/esp/openocd
+OPENOCD_ESP32_CONF_OPTS += --prefix=/usr/local/esp/openocd
 OPENOCD_ESP32_CONF_OPTS += --enable-ftdi
-OPENOCD_ESP32_CONF_OPTS += $(OPENOCD_ESP32_CFLAGS)
-OPENOCD_ESP32_CONF_OPTS += --host=arm-unknown-linux-gnueabihf
 OPENOCD_ESP32_CONF_OPTS += --disable-doxygen-html
 OPENOCD_ESP32_CONF_OPTS += --includedir=$(STAGING_DIR)/usr/include/libusb-1.0
+OPENOCD_ESP32_CONF_OPTS += --with-jim-shared=no
+OPENOCD_ESP32_CONF_OPTS += --disable-shared
+OPENOCD_ESP32_CONF_OPTS += --enable-dummy
+OPENOCD_ESP32_CONF_OPTS += --disable-werror
 
-
-define OPENOCD_ESP32_CONFIGURE_CMDS
-	(cd $(@D); $(TARGET_MAKE_ENV) ./bootstrap nosubmodule )
-	(cd $(@D); $(TARGET_MAKE_ENV) ./configure $(TARGET_CONFIGURE_OPTS) $(OPENOCD_ESP32_CONF_OPTS) )
-endef
-
-define OPENOCD_ESP32_BUILD_CMDS
-	$(MAKE) CFLAGS="-Wno-error -I$(TARGET_DIR)/usr/include/libusb-1.0" -C $(@D)
-endef
-
-define OPENOCD_ESP32_INSTALL_TARGET_CMDS
-	$(MAKE) CFLAGS="-Wno-error -I$(TARGET_DIR)/usr/include/libusb-1.0" -C $(@D) install
-endef
-
-$(eval $(generic-package))
+$(eval $(autotools-package))
