fa67750e914e76ff080e1f85face04fbd538ad1d
Eric Rauer <eric.rauer+gh@gmail.com>
2019-05-06 15:24:51 -0700

    Add libfreefare from master

---
+++ package/libfreefare_ly/libfreefare_ly.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# libfreefare-ly
+#
+################################################################################
+LIBFREEFARE_LY_VERSION = 5459806659d5f6aa0e7705b88c48775ea6c861a6
+LIBFREEFARE_LY_SITE = git://github.com/nfc-tools/libfreefare
+
+LIBFREEFARE_LY_DEPENDENCIES = libnfc openssl
+LIBFREEFARE_LY_LICENSE = LGPL-3.0+ with exception
+LIBFREEFARE_LY_LICENSE_FILES = COPYING
+
+ifeq ($(BR2_STATIC_LIBS),y)
+# openssl needs zlib even if the libfreefare example itself doesn't
+LIBFREEFARE_LY_CONF_ENV += LIBS='-lz'
+endif
+
+$(eval $(autotools-package))
