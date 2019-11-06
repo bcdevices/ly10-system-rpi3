539bdbbf18a2c7e504a0a588dda89b886e7373cf
Eric Rauer <eric.rauer+gh@gmail.com>
2019-11-06 09:10:56 -0800

    Use existing makefile

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-mkdir -p "$HOME/.nerves/dl"
-cp -R vendor/* "$HOME/.nerves/dl"
-mix local.hex --force
-mix archive.install hex nerves_bootstrap 1.6.1 --force
-mix deps.get
-mix compile
-mkdir ./dist
-if [ -f "$HOME/.nerves/dl/ly10_system_rpi3-portable-*" ]
-then
-  cp "$HOME/.nerves/dl/ly10_system_rpi3-portable-*" ./dist
-else
-  fakeroot mix nerves.artifact ly10_system_rpi3 --path ./dist
-fi
+make dist
