b8468b6b484528df2a989f22a4994960fb932792
Eric Rauer <eric.rauer+gh@gmail.com>
2020-02-20 13:53:41 -0800

    Add yubico piv tool

---
+++ package/yubico_piv_tool/yubico_piv_tool.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# yubico_piv_tool
+#
+################################################################################
+
+YUBICO_PIV_TOOL_VERSION = yubico-piv-tool-2.0.0
+YUBICO_PIV_TOOL_SITE = git://github.com/Yubico/yubico-piv-tool.git
+YUBICO_PIV_TOOL_LICENSE = BSD
+YUBICO_PIV_TOOL_LICENSE_FILES = COPYING
+
+YUBICO_PIV_TOOL_AUTORECONF = YES
+YUBICO_PIV_TOOL_DEPENDENCIES = libtool
+
+$(eval $(autotools-package))
