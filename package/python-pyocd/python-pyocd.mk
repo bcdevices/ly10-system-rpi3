4e2eec2f2ededb7130a666c1a095eed3c9f70c60
Eric Rauer <eric.rauer+gh@gmail.com>
2020-04-07 14:06:54 -0700

    Initial pyOCD package

---
+++ package/python-pyocd/python-pyocd.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-pyocd
+#
+################################################################################
+
+PYTHON_PYOCD_VERSION = 0.25.0
+PYTHON_PYOCD_SOURCE = $(PYTHON_PYOCD_VERSION).tar.gz
+PYTHON_PYOCD_SITE = git://github.com/mbedmicro/pyOCD
+PYTHON_PYOCD_SETUP_TYPE = setuptools
+PYTHON_PYOCD_LICENSE = Apache-2.0
+PYTHON_PYOCD_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
