0cb996f22271a90b747a1716817bfecaaf083250
Eric Rauer <eric.rauer+gh@gmail.com>
2020-04-15 18:12:05 -0700

    Change pyocd download method

--- package/python-pyocd/python-pyocd.mk
+++ package/python-pyocd/python-pyocd.mk
@@ -3,10 +3,8 @@
 # python-pyocd
 #
 ################################################################################
-PYTHON_PYOCD_VERSION = 0.25.0
-PYTHON_PYOCD_SOURCE = v$(PYTHON_PYOCD_VERSION).tar.gz
-PYTHON_PYOCD_SOURCE = pyocd-$(PYTHON_PYOCD_VERSION).tar.gz
-PYTHON_PYOCD_SITE = git://github.com/mbedmicro/pyOCD
+PYTHON_PYOCD_VERSION = v0.25.0
+PYTHON_PYOCD_SITE = $(call github,mbedmicro,pyOCD,$(PYTHON_PYOCD_VERSION))
 PYTHON_PYOCD_SETUP_TYPE = setuptools
 PYTHON_PYOCD_LICENSE = Apache-2.0
 PYTHON_PYOCD_LICENSE_FILES = LICENSE
