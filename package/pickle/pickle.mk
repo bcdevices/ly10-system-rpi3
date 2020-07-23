0f1d61b130546b06853bfdc8828558f571632c13
Ivo Clarysse <ivo@bcdevices.com>
2020-07-23 15:48:35 +0200

    picke: generic-package

--- package/pickle/pickle.mk
+++ package/pickle/pickle.mk
@@ -19,4 +19,38 @@ else ifeq ($(BR2_PACKAGE_LIBFTDI),y)
 PICKLE_DEPENDENCIES += libftdi
 endif
 
+define PICKLE_BUILD_CMDS
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
 
+#define PICKLE_INSTALL_STAGING_CMDS
+#    $(INSTALL) -D -m 0755 $(@D)/libfoo.a $(STAGING_DIR)/usr/lib/libfoo.a
+#    $(INSTALL) -D -m 0644 $(@D)/foo.h $(STAGING_DIR)/usr/include/foo.h
+#    $(INSTALL) -D -m 0755 $(@D)/libfoo.so* $(STAGING_DIR)/usr/lib
+#endef
+
+define PICKLE_INSTALL_TARGET_CMDS
+    $(INSTALL) -d -g0 -m0755 -o0 $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 4755 $(@D)/pickle $(TARGET_DIR)/usr/bin
+    ln -sf $(TARGET_DIR)/pickle $(TARGET_DIR)/p12
+    ln -sf $(TARGET_DIR)/pickle $(TARGET_DIR)/p14
+    ln -sf $(TARGET_DIR)/pickle $(TARGET_DIR)/n14
+    ln -sf $(TARGET_DIR)/pickle $(TARGET_DIR)/p16
+    ln -sf $(TARGET_DIR)/pickle $(TARGET_DIR)/n16
+    ln -sf $(TARGET_DIR)/pickle $(TARGET_DIR)/p24
+    ln -sf $(TARGET_DIR)/pickle $(TARGET_DIR)/p32
+    $(INSTALL) -D -m 4755 $(@D)/pio $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 4755 $(@D)/ptest $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 4755 $(@D)/pctrl $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 4755 $(@D)/pload $(TARGET_DIR)/usr/bin
+endef
+
+#define PICKLE_USERS
+#    foo -1 libfoo -1 * - - - LibFoo daemon
+#endef
+
+#define PICKLE_PERMISSIONS
+#    /bin/foo  f  4755  foo  libfoo   -  -  -  -  -
+#endef
+
+$(eval $(generic-package))
