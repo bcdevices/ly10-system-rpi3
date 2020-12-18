4d604efb31da01aa6fabfebd1def798ac7c68d9f
Eric Rauer <github@rauer.dev>
2020-12-18 10:41:19 -0800

    Fix check for var directories

--- post-build.sh
+++ post-build.sh
@@ -6,16 +6,16 @@ set -e
 # active firmware.
 mkdir -p "$TARGET_DIR/usr/share/fwup"
 
-if [ -f /root/local ]; then
+if [ -d /root/local ]; then
   ln -sf /root/local "$TARGET_DIR/var/local"
 fi
 
-if [ -f /root/empty ]; then
+if [ -d /root/empty ]; then
   ln -sf /root/empty "$TARGET_DIR/var/empty"
 fi
 
 
-$HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-revert.conf -o $TARGET_DIR/usr/share/fwup/revert.fw
+"$HOST_DIR/usr/bin/fwup" -c -f "$NERVES_DEFCONFIG_DIR/fwup-revert.conf" -o "$TARGET_DIR/usr/share/fwup/revert.fw"
 
 # Copy the fwup includes to the images dir
-cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
+cp -rf "$NERVES_DEFCONFIG_DIR/fwup_include" "$BINARIES_DIR"
